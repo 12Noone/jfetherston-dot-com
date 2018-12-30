3596c19f096b7905b4416bf69e93c2a6706f2027
Jill Noone <jnoone@zola.com>
2018-12-29 21:52:17 -0500

    refactoring viewer a bit

--- src/components/Viewer.js
+++ src/components/Viewer.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from 'prop-types';
+import _ from 'lodash';
+import { projectData } from '../viewerOptionsData';
 import "../stylesheets/Viewer.less";
 
-const Viewer = ({ handleClose, show, viewerOptions }) => {
-  const showHideClassName = show ? "viewer active" : "viewer";
-  return (
-    <div className={showHideClassName}>
+
+class Viewer extends Component {
+  constructor(props) {
+    super(props);
+    this.renderTech = this.renderTech.bind(this);
+    this.renderCopy = this.renderCopy. bind(this);
+  }
+
+  renderTech(viewerOptions, techItems) {
+    if (_.isEmpty(viewerOptions)) {
+      return;
+    }
+    return (
+      techItems.map((tech, idx) => (
+        <div key={idx} className="tech">{tech}</div>
+      ))
+    );
+  }
+
+  renderCopy(viewerOptions) {
+    return (
+      <div>
+        <h1>{viewerOptions.header}</h1>
+        <h5>{viewerOptions.subHeading}</h5>
+        <p>{viewerOptions.bodyCopy}</p>
+      </div>
+    )
+  }
+  render() {
+    const { handleClose, show, viewerOptions } = this.props;
+    const showHideClassName = show ? "viewer active" : "viewer";
+    return (
+      <div className={showHideClassName}>
       <section className="viewer-main">
         <div className="close-viewer" onClick={handleClose}>
           X
@@ -21,16 +52,20 @@ const Viewer = ({ handleClose, show, viewerOptions }) => {
                 null
               )}
               <div className="copy-container">
-                <h1>{viewerOptions.header}</h1>
-                <h5>{viewerOptions.subHeading}</h5>
-                <p>{viewerOptions.bodyCopy}</p>
+                { this.renderCopy(viewerOptions) }
+              </div>
+              <div className="tech-container">
+                <h3>Tech Used:</h3>
+                { this.renderTech(viewerOptions, viewerOptions.tech) }
               </div>
             </div>
           </div>
         </div>
       </section>
     </div>
-  );
+    )
+
+  }
 };
 
 Viewer.propTypes = {
